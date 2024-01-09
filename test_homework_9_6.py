15fb4676e68a60b873ff983f5bca4ad6b1ecdd7f
Evgeny Zelenin <evgeny.globe@yandex.ru>
2024-01-09 17:36:44 +0300

    Сделал часть заданий

--- test_homework_9_6.py
+++ test_homework_9_6.py
@@ -1,3 +1,4 @@
+import datetime
 from datetime import time
 
 
@@ -10,7 +11,7 @@ def test_dark_theme_by_time():
 
     is_dark_theme = None
 
-    if time(hour=6) < current_time < time(hour=22):
+    if datetime.time(hour=6) < current_time < datetime.time(hour=22):
         is_dark_theme = False
     else:
         is_dark_theme = True
